037bd4d606cdd372909fdf199e65cb0332aa7418
njacobs5074 <nicholas_jacobs@yahoo.com>
2015-02-10 18:32:28 +0000

    Made some simple changes so that client uses resource links in the messages instead of hard-coding them.

--- qzr-web/src/main/webapp/js/services.js
+++ qzr-web/src/main/webapp/js/services.js
@@ -4,100 +4,132 @@
 
 var qzrServices = angular.module('qzr.services', ['ngResource']);
 
-qzrServices.factory('qzrSvc', ['$http', function($http) { 
+qzrServices.factory('qzrSvc', ['$http', function ($http) {
 
-	var svc = {
-    	
-		initialised: false,
-    	eventListeners: [],
-		socket : null,
-		stompClient : null,
-		isConnecting : false,
-		isConnected : false,
+    var RESOURCES = '/api/quizzes/health/';
 
-        model : {
-            questions : [],
-            knowns : [],
+    function getKeyResourceHref(resource) {
+        for (var i = 0; i < resource.links.length; i++) {
+            if (resource.links[i].rel !== 'self')
+                return resource.links[i].href;
+        }
+        return null;
+    }
+
+    var svc = {
+
+        initialised: false,
+        eventListeners: [],
+        socket: null,
+        stompClient: null,
+        isConnecting: false,
+        isConnected: false,
+        restInterfaces: {},
+
+        model: {
+            questions: [],
+            knowns: [],
             hrmax: null,
             events: []
         },
 
-        connect: function() {
-        	// If for some reason this gets called twice, we try to avoid connecting again.
-        	if (svc.isConnecting || svc.isConnected) return;
+        connect: function () {
+            // If for some reason this gets called twice, we try to avoid connecting again.
+            if (svc.isConnecting || svc.isConnected) return;
 
-        	svc.isConnecting = true;
+            svc.isConnecting = true;
 
-			// Assumes that service is on same server and that we have been consistent with ports.
-			svc.socket = new SockJS('/drools');
-			svc.stompClient = Stomp.over(svc.socket);
-			svc.stompClient.connect({}, function(frame) {
-				svc.isConnected = true;
-				svc.isConnecting = false;
-				console.log('Connected to /drools : ' + frame);
-				svc.stompClient.subscribe('/queue/agendaevents/', svc.newEvent );
-			});
-        },
-        
-        newEvent: function(data) {
-			var event = JSON.parse(data.body);
-			svc.model.events.push(event);
+            // Assumes that service is on same server and that we have been consistent with ports.
+            svc.socket = new SockJS('/drools');
+            svc.stompClient = Stomp.over(svc.socket);
+            svc.stompClient.connect({}, function (frame) {
+                svc.isConnected = true;
+                svc.isConnecting = false;
+                console.log('Connected to /drools : ' + frame);
+                svc.stompClient.subscribe('/queue/agendaevents/', svc.newEvent);
+            });
+
         },
 
-        disconnect: function() {
+        newEvent: function (data) {
+            var event = JSON.parse(data.body);
+            svc.model.events.push(event);
+        },
+
+        disconnect: function () {
             svc.stompClient.disconnect();
             svc.setConnected(false);
             console.log("Disconnected Drools working memory event listener client");
         },
-        
-        loadQuestions: function() {
-            $http.get('/api/quizzes/health/questions')
-	            .success(function(questions) { 
-	            	svc.model.questions = questions;
-	    	        $http.get('/api/quizzes/health/results/hrmax')
-		        		.success(function(hrmax) {
-		        			svc.model.hrmax = hrmax;
-				        	$http.get('/api/quizzes/health/knowns')
-							.success(function(knowns) {
-								svc.model.knowns = knowns;
-							});
-		        		})
-		        		.error(function() { svc.model.hrmax = null; });
-	            });
-        },
-        
-       
-        init: function() {
-        	if (!svc.initialised) svc.loadQuestions();
-        	svc.connect();
+
+        loadResources: function () {
+
+            // Load our REST interface links from the server.
+            $http.get(RESOURCES)
+                .success(function (data) {
+                    for (var i = 0; i < data.links.length; i++) {
+
+                        if (data.links[i].rel === "self")
+                            continue;
+
+                        svc.restInterfaces[data.links[i].rel] = data.links[i].href;
+                    }
+
+                    console.log('Loading from ' + svc.restInterfaces['questions'] + '...');
+                    $http.get(svc.restInterfaces['questions'])
+                        .success(function (questions) {
+                            svc.model.questions = questions;
+                            $http.get(svc.restInterfaces['hrmax'])
+                                .success(function (hrmax) {
+                                    svc.model.hrmax = hrmax;
+                                    $http.get(svc.restInterfaces['knowns'])
+                                        .success(function (knowns) {
+                                            svc.model.knowns = knowns;
+                                        });
+                                })
+                                .error(function () {
+                                    svc.model.hrmax = null;
+                                });
+                        })
+                        .error(function () {
+                            throw Error('Failed to retrieve ' + svc.restInterfaces['questions']);
+                        })
+                })
+                .error(function () {
+                    throw Error('Failed to retrieve ' + RESOURCES);
+                });
         },
 
-		answer : function(question, answerValue) {
-        	console.log("Answering : " + question.key + "=" + answerValue);
-        	
-        	var answer = { key: question.key, value: answerValue };
-            
-            $http.put('/api/quizzes/health/questions/' + question.key + '/answer', answer)
-                 .success(svc.loadQuestions);
+
+        init: function () {
+            if (!svc.initialised) {
+                svc.loadResources();
+            }
+            svc.connect();
         },
 
-        skip : function(question) {
-        	console.log("Skipping : " + question.key);
-        	
-            $http.post('/api/quizzes/health/questions/' + question.key + '/skip')
-                 .success(svc.loadQuestions);
+        answer: function (question, answerValue) {
+            console.log("Answering : " + question.key + "=" + answerValue);
+
+            var answer = {key: question.key, value: answerValue};
+
+            $http.put(getKeyResourceHref(question), answer).success(svc.loadResources);
         },
 
-        retractAnswer : function(question) {
-        	console.log("Retracting : " + question.key);
-        	
-            $http.delete('/api/quizzes/health/questions/' + question.key + '/answer')
-                 .success(svc.loadQuestions);
-        } 
+        skip: function (question) {
+            console.log("Skipping : " + question.key);
 
+            $http.post(getKeyResourceHref(question)).success(svc.loadResources);
+        },
+
+        retractAnswer: function (question) {
+            console.log("Retracting : " + question.key);
+
+            $http.delete(getKeyResourceHref(question)).success(svc.loadResources);
+        }
     };
-    
+
     return svc;
-    
+
 }]);
 
